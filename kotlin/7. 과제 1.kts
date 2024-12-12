e3837a38b534f310f5970fd70cf3b2c7a9bcec1c
jsw9436 <jswjsw101010@gmail.com>
2024-12-12 19:45:44 +0900

    Fastcampus

--- kotlin/7. 과제 1.kts
+++ kotlin/7. 과제 1.kts
@@ -11,5 +11,15 @@ println(BB)
 // 3번
 fun checkGrade(String:Int) : String {
     if (score >= 90) return "A학점"
-    else if(score)
+    else if(score >= 80) return "B학점"
+    else if(score >= 70) return "C학점"
+    else return "F학점"
+    when(score){
+        in 90..100 -> return "A학점"
+        in 80..89 -> return "B학점"
+        in 70..70 -> return "C학점"
+        else -> return "F학점"
+    }
 }
+val result: String = checkGrade(score:90)
+        println(result)
