bb04030c41b63aa4c704ff0ac7b1c8738936b404
jsw9436 <jswjsw101010@gmail.com>
2025-01-11 22:21:26 +0900

    Fastcampus

--- kotlin/8. 배열/8.배열 실습.kts
+++ kotlin/8. 배열/8.배열 실습.kts
@@ -11,3 +11,26 @@ println(array[4])
 println(array[6])
 var array5 = IntArray(size:10, { 0 })
 //var array6 = String(10, {"Hi"}) // -> 불가능
+
+// 배열을 선언하는 방법 (3)
+var array6 = Array<Int>(size:10, { 0 })
+var array7 = Array<String>(sie:10, { "" })
+
+var array8 = arrayOf<Int>(10, 20, 30, 40)
+println(array8[2])
+println(array8=get(2))
+
+array8[0] = 100
+println(array8[0])
+
+array8.set(0, 200)
+println(array8[0])
+
+// 배열의 인자를 변수에 넣는 방법 / 배열의 값을 변수의 값으로 할당하는 방법
+val examScore1: Int = 100
+val examScore2: Int = 90
+val examScore3: Int = 80
+var examScores = arrayOf<Int>(examScore1, examScore2, examScore3)
+
+val examScore4 = examScores[0]
+println(examScore4)
