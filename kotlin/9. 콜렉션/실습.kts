4dff9aa9acb35746c2bef60deb0be22a7b806d42
jsw9436 <jswjsw101010@gmail.com>
2025-01-24 01:26:08 +0900

    Fastcampus

--- kotlin/9. 콜렉션/실습.kts
+++ kotlin/9. 콜렉션/실습.kts
@@ -2,3 +2,7 @@
 val numbers = listOf<Int>(1, 2, 3)
 val numbers2 = mutableListOf<Int>(1, 2, 3)
 println(numbers2)
+
+// Set
+val numbers3 = SetOf<Int>(1, 1, 1, 3, 3, 4)
+val numbers4 = mutableListOf<Int>(1, 1, 1, 3, 3, 4)
